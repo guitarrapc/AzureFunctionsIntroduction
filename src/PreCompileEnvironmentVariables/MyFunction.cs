077e73fda86c748970a992718ef5bc0b8a4ec6c6
guitarrapc <cherishacquire@outlook.com>
2017-01-11 03:38:39 +0900

    Shorten FunctionName

--- src/PreCompileEnvironmentVariables/MyFunction.cs
+++ src/PreCompileEnvironmentVariables/MyFunction.cs
@@ -6,7 +6,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace PreCompileEnvironmentVariablesWebhookCSharp
+namespace PreCompileEnvironmentVariables
 {
     public class MyFunction
     {
