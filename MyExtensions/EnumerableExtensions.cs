608d7f55bb86f8a066a2da7c2240cb558220687a
guitarrapc <cherishacquire@outlook.com>
2017-01-20 05:44:56 +0900

    MyExtensions will be generated on CD.

--- MyExtensions/EnumerableExtensions.cs
+++ MyExtensions/EnumerableExtensions.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#pragma warning disable 1587
+/// <summary>
+/// Any build package for this solution will be output to $(SolutionDir)\sharedlib\*.dll to prepare other .csx reference.
+/// MSBuild will be run on Continuous Deployment with AzureFunction via Kudo using ".deployment".
+/// </summary>
+#pragma warning restore 1587
 namespace MyExtensions
 {
     public static class EnumerableExtensions
