f2860339f84b8fbe4edf54f4e3adc5ad17ab8124
guitarrapc <cherishacquire@outlook.com>
2017-02-17 04:00:32 +0900

    update deployment

--- deploy.cmd
+++ deploy.cmd
@@ -75,9 +75,7 @@ FOR /F %%d in ('DIR "*.sln" /S /B') DO (
 :: MSBuild
 echo "MSBuild solution"
 FOR /F %%d in ('DIR "src\*.csproj" /S /B') DO (
-setlocal
    call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\src\%%~nd\%%~nd.csproj" /nologo /verbosity:m /t:Build /p:Configuration=Release;OutputPath="%DEPLOYMENT_TEMP%\%%~nd";UseSharedCompilation=false %SCM_BUILD_ARGS%
-endlocal
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
@@ -87,16 +85,14 @@ endlocal
 
 :: NuGet package restore
 echo "Restoring function packages"
-
 FOR /F %%d in ('DIR "Project.json" /S /B') DO ( 
   call nuget restore %%d -PackagesDirectory %home%\data\Functions\packages\nuget
 )
 
 echo Handling Basic Web Site deployment.
-
 :: KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;Project.json"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;host.json"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
