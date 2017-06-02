6725175aa05043c10e487ea061b074cb26bf4f80
guitarrapc <cherishacquire@outlook.com>
2017-06-03 00:58:04 +0900

    Remove Non usable sample. Change Project type from VS2017 to 2015 for Class Library

--- src/CSharpScripting/Properties/AssemblyInfo.cs
+++ src/CSharpScripting/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CSharpScripting")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b2e77f45-ee5d-4d93-b447-e27ccdb16794")]
+[assembly: Guid("6504b65e-1f79-432c-b73f-aeb9e6976d1d")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- src/_CSharpScripting/RoslynCompiler.cs
+++ src/_CSharpScripting/RoslynCompiler.cs
@@ -1,18 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using MyExtensions;
 
-namespace CSharpCompilerSlackOuthookCSharp
+namespace CSharpScripting
 {
-    public class CSharpScripting
+    public class RoslynCompiler
     {
         private static readonly string[] DefaultImports = new[] {
             "System",
