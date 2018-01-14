b9bfd1d6cda183be14415d7239f2ab9bcb2b9136
Ikiru Yoshizaki <cherishacquire@outlook.com>
2018-01-15 04:56:32 +0900

    Add(V2) Add GenericWebhookCSharpExtensionMethod Function

---
+++ src2/AzureFunctionsIntroduction/AppSettingsWebhookCSharp.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System;
+
+namespace AzureFunctionsIntroduction
+{
+    public static class AppSettingsWebhookCSharp
+    {
+        [FunctionName("AppSettingsWebhookCSharp")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("AppSettingsWebhookCSharp : C# HTTP trigger function processed a request.");
+
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<Input>(jsonContent);
+
+            // You can access Azure Functions Portal > Application Settings setting variable.
+            var envKey = data.key;
+            var envValue = Environment.GetEnvironmentVariable(envKey);
+
+            return req.CreateResponse(HttpStatusCode.OK, new
+            {
+                key = envKey,
+                value = envValue,
+            });
+        }
+
+        private class Input
+        {
+            public string key { get; set; }
+        }
+    }
+}
---
+++ src2/AzureFunctionsIntroduction/GenericWebhookCSharpExtensionMethod.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace AzureFunctionsIntroduction
+{
+    public static class GenericWebhookCSharpExtensionMethod
+    {
+        [FunctionName("GenericWebhookCSharpExtensionMethod")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("GenericWebhookCSharpExtensionMethod : C# HTTP trigger function processed a request.");
+
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            dynamic input = JsonConvert.DeserializeObject(jsonContent);
+            var start = int.Parse((string)input.start);
+            var range = int.Parse((string)input.range);
+
+            var extensionMethodTest = Enumerable.Range(start, range).Select(x => x * 10).ToArray().ToJoinedString(",");
+            return req.CreateResponse(HttpStatusCode.OK, new
+            {
+                Result = $"{extensionMethodTest}"
+            });
+        }
+    }
+}
