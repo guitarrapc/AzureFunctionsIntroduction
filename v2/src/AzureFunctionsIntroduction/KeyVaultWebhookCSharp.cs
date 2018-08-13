2a5a46eed4e26ba386b3d54340682de3327eb21a
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2018-08-13 13:41:20 +0900

    KeyVault need SecretUri

--- v2/src/AzureFunctionsIntroduction/KeyVaultWebhookCSharp.cs
+++ v2/src/AzureFunctionsIntroduction/KeyVaultWebhookCSharp.cs
@@ -20,75 +20,16 @@ namespace AzureFunctionsIntroduction
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
-            return await Execute(req);
-        }
-
-        private static async Task<HttpResponseMessage> Execute(HttpRequestMessage req)
-        {
-            switch (req.Method.Method)
-            {
-                case "POST":
-                    return await PostHandler(req);
-                case "GET":
-                    return await GetHandler(req);
-                default:
-                    throw new NotImplementedException();
-            }
-        }
-
-        private static async Task<HttpResponseMessage> PostHandler(HttpRequestMessage req)
-        {
-            string jsonContent = await req.Content.ReadAsStringAsync();
-            var data = JsonConvert.DeserializeObject<Input>(jsonContent);
-
-            // You can access Azure Functions Portal > Application Settings setting variable.
-            var envKey = data.key;
-            var secret = await GetKeyVaultSecret(envKey);
-
-            return req.CreateResponse(HttpStatusCode.OK, new
-            {
-                key = envKey,
-                value = secret,
-            });
-        }
-
-        private static async Task<HttpResponseMessage> GetHandler(HttpRequestMessage req)
-        {
-            var input = new Input();
-            var properties = typeof(Input).GetProperties().Select(x => x.Name).ToArray();
-            var keyValues = req.GetQueryNameValuePairs().Where(x => properties.Contains(x.Key));
-            if (!keyValues.Any()) throw new NullReferenceException("No query Parameter 'key' not found.");
-
-            // only check first key
-            var keyValue = keyValues.First();
-            var property = typeof(Input).GetProperty(keyValue.Key);
-            property.SetValue(input, keyValue.Value);
-
-            // You can access Azure Functions Portal > Application Settings setting variable.
-            var envKey = input.key;
-            var secret = await GetKeyVaultSecret(envKey);
-
-            return req.CreateResponse(HttpStatusCode.OK, new
-            {
-                key = envKey,
-                value = secret,
-            });
-        }
-
-        private static async Task<string> GetKeyVaultSecret(string envKey)
-        {
             // Make sure AppService can access to KeyVault by
             // 1. Enable Managed Service Identity : AppService
             // 2. Enable AccessPolicy for AppService. : KeyVault
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
             var kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback), client);
-            var secret = (await kvClient.GetSecretAsync(envKey)).Value;
-            return secret;
-        }
-
-        private class Input
-        {
-            public string key { get; set; }
+            var secret = (await kvClient.GetSecretAsync(Environment.GetEnvironmentVariable("KeyVaultSecretUri"))).Value;
+            return req.CreateResponse(HttpStatusCode.OK, new
+            {
+                Value = secret,
+            });
         }
     }
 }
