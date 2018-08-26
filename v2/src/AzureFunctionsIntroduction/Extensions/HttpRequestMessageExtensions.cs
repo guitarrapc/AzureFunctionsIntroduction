258c61668dae9079c5d852cdccc0c2453f42b869
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2018-08-27 01:27:13 +0900

    add(v2) explanation

--- v2/src/AzureFunctionsIntroduction/Extensions/HttpRequestMessageExtensions.cs
+++ v2/src/AzureFunctionsIntroduction/Extensions/HttpRequestMessageExtensions.cs
@@ -11,11 +11,20 @@ namespace AzureFunctionsIntroduction
 {
     public static class HttpRequestMessageExtensions
     {
+        // Hack : GetQueryNameValuePairs not yet umplemented with AzureFunctions v2 default.
         public static Dictionary<string, StringValues> GetQueryNameValuePairs(this HttpRequestMessage req)
         {
             return Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(req.RequestUri.Query);
         }
 
+        // Hack : existing CreateResponse throw exception. Issue : https://github.com/Azure/azure-webjobs-sdk/issues/1492
+        /// <summary>
+        /// Custom CreateResponse to for sugar syntax...
+        /// </summary>
+        /// <param name="res"></param>
+        /// <param name="statusCode"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static HttpResponseMessage CreateResponse(this HttpRequestMessage res, HttpStatusCode statusCode, string value)
         {
             if (res == null) throw new ArgumentNullException(nameof(res));
@@ -26,6 +35,14 @@ namespace AzureFunctionsIntroduction
             };
         }
 
+        /// <summary>
+        /// Custom CreateResponse to for sugar syntax...
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="req"></param>
+        /// <param name="statusCode"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static HttpResponseMessage CreateResponse<T>(this HttpRequestMessage req, HttpStatusCode statusCode, T value) where T : class
         {
             if (req == null) throw new ArgumentNullException(nameof(req));
@@ -37,6 +54,13 @@ namespace AzureFunctionsIntroduction
             };
         }
 
+        /// <summary>
+        /// Custom CreateErrorResponse to for sugar syntax... no concider for HttpError
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="statusCode"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static HttpResponseMessage CreateErrorResponse(this HttpRequestMessage req, HttpStatusCode statusCode, string value)
         {
             if (req == null) throw new ArgumentNullException(nameof(req));
