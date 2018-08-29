82eb0ccf9ea377b59596a46c1ca215454e8839c4
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2018-08-30 03:21:22 +0900

    add(v2) CSharpCompilerWebhookCSharp

---
+++ v2/src/AzureFunctionsIntroduction/Features/RoslynComplier.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionsIntroduction.Features
+{
+    public class RoslynComplier
+    {
+        private static readonly string[] DefaultImports = new[] {
+            "System",
+            "System.IO",
+            "System.Linq",
+            "System.Collections.Generic",
+            "System.Text",
+            "System.Text.RegularExpressions",
+            "System.Net",
+            "System.Net.Http",
+            "System.Threading",
+            "System.Threading.Tasks",
+        };
+
+        private static readonly Assembly[] DefaultReferences =
+        {
+            typeof(Enumerable).Assembly,
+            typeof(List<string>).Assembly,
+            typeof(HttpClient).Assembly,
+        };
+
+        public static async Task<string> EvaluateCSharpAsync(string code)
+        {
+            try
+            {
+                var result = await CSharpScript.EvaluateAsync(code ?? "Input any code, please?",
+                    ScriptOptions.Default
+                        .WithImports(DefaultImports)
+                        .WithReferences(new[] {
+                            "System",
+                            "System.Core",
+                            "System.Xml",
+                            "System.Xml.Linq",
+                        })
+                        .WithReferences(DefaultReferences));
+                return result?.ToString() ?? "";
+            }
+            catch (Exception ex)
+            {
+                return $"{ex.Message}{Environment.NewLine}{ex.StackTrace}";
+            }
+        }
+    }
+}
---
+++ v2/src/AzureFunctionsIntroduction/RoslynCompilerServiceCSharp.cs
@@ -0,0 +1,47 @@
+﻿using AzureFunctionsIntroduction.Features;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionsIntroduction
+{
+    // request : {"code" : "Enumerable.Range(0, 10).Select(x => x * x).Sum()"}
+    // should be : {"body":"285"}
+    public class RoslynCompilerServiceCSharp
+    {
+        [FunctionName("CSharpCompilerWebhookCSharp")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"CSharpCompilerWebhookCSharp : C# HTTP trigger function processed a request.");
+
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(jsonContent);
+
+            if (data == null || data.code == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, new
+                {
+                    error = "missing <code> property."
+                });
+            }
+
+            // Evaluate CSharp Code
+            string code = data.code;
+            log.Info($"{nameof(code)} : {code}");
+            var resultText = await RoslynComplier.EvaluateCSharpAsync(code);
+            log.Info(resultText);
+
+            return req.CreateResponse(HttpStatusCode.OK, new
+            {
+                body = resultText,
+            });
+        }
+    }
+}
